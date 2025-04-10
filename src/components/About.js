01f147570d0f9d32050fe64ee4144ab20d938834
Njuguna <joshuangich2@gmail.com>
2025-04-10 06:31:55 +0300

    Initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,19 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        A paragraph is defined as “a group of sentences or a single sentence
+        that forms a unit” (Lunsford and Connors 116). Length and appearance do
+        not determine whether a section in a paper is a paragraph. For instance,
+        in some styles of writing, particularly journalistic styles, a paragraph
+        can be just one sentence long.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
